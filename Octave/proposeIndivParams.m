d96af9b3609fc43337d2ed84b474c8e2f0fbe785
magStra <mes68@cam.ac.uk>
2018-05-29 12:46:05 +0100

    checking Octave compatibility

--- Octave/proposeIndivParams.m
+++
@@ -1,4 +0,0 @@
-function proposedIndivParams = proposeIndivParams(currentLogParams,stepSize,nGenes)
-%sampling parameters for pseudoRankIndivParams   
-proposedIndivParams = currentLogParams + stepSize.*randn([3 nGenes]);
-end
--- Octave/proposeParams.m
+++
@@ -1,7 +0,0 @@
-function proposedParams = proposeParams(currentLogParams,stepSize,varData)
-%sampling parameters for pseudoRank
-    proposedParams = currentLogParams + (stepSize.*randn([1 2]))';
-    if proposedParams(1) > log(sqrt(varData))
-        proposedParams(1) = log(sqrt(varData) - 0.05);
-    end
-end
